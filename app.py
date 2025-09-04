980db810d2543948fb243dd2f15be6811579ee3b
shiv santosh <shivsantosh04@gmail.com>
2025-09-04 10:03:57 +0530

    updated app.py to run production server

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -43,5 +44,8 @@ def home():
     return "ML Model API is running!"
 
 # This is needed for local testing
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
